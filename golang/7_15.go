08093356562bb1360b690686fd8e759844d2e9b7
Ahad-UCF <Ahadbawany@knights.ucf.edu>
2019-07-15 22:36:31 -0400

    Clarified code changes for 7/15

--- golang/7_15.go
+++ golang/7_15.go
@@ -1,13 +1,13 @@
 // Ahad Bawany
 // Daily code 7/15/2019:
 // Given an order id, implement functions to get an item from the log or add an item to the log
-// For simplicitiy's sake, I've chosen to do this program using the built in linked list library
+// For simplicitiy's sake, I've chosen to do this program using the built in slices
 
 package main
 
 import (
 	"fmt"
-	//"container/list"
+
 )
 
 // For simplicity's sake, the order ID is assumed to be an integer. This can easily be modified
