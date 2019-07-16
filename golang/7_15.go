9187d71374d6df1ccd705d89ab5f244f1d6c685c
Ahad-UCF <Ahadbawany@knights.ucf.edu>
2019-07-15 22:35:08 -0400

    Added 7/15's solution for golang!

---
+++ golang/7_15.go
@@ -0,0 +1,32 @@
+// Ahad Bawany
+// Daily code 7/15/2019:
+// Given an order id, implement functions to get an item from the log or add an item to the log
+// For simplicitiy's sake, I've chosen to do this program using the built in linked list library
+
+package main
+
+import (
+	"fmt"
+	//"container/list"
+)
+
+// For simplicity's sake, the order ID is assumed to be an integer. This can easily be modified
+func record(sli []int, n int )([]int){
+	sli = append(sli, n)
+	return sli
+}
+
+// Returns the ith LAST id
+func get_last(sli []int, i int)(int){
+	return sli[len(sli) - i]
+}
+
+
+func main(){
+	sli := []int{}
+	sli = record(sli, 30202)
+	sli = record(sli, 4020)
+	sli = record(sli, 5)
+	sli = record(sli, 811)
+	fmt.Println(get_last(sli, 2))
+}
